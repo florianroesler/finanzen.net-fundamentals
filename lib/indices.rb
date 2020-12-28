45575a36e68d13fbf988609ac0a8e4b873c0a48c
Florian Rösler <florian.roesler@datarade.net>
2020-12-28 15:41:39 +0100

    Added more markets

--- lib/indices.rb
+++ lib/indices.rb
@@ -8,6 +8,19 @@ INDICES = {
   us: [
     'https://www.finanzen.net/index/dow_jones/werte',
     'https://www.finanzen.net/index/nasdaq_100/werte',
-    'https://www.finanzen.net/index/s&p_500/werte'
+    'https://www.finanzen.net/index/s&p_500/werte',
+    'https://www.finanzen.net/index/s&p_600_small_cap/werte'
+  ],
+  eu: [
+    'https://www.finanzen.net/index/ftse_100/werte',
+    'https://www.finanzen.net/index/cac_40/werte',
+    'https://www.finanzen.net/index/ibex_35/werte',
+    'https://www.finanzen.net/index/smi/werte',
+    'https://www.finanzen.net/index/euro_stoxx_50/werte'
+  ],
+  asia:
+  [
+    'https://www.finanzen.net/index/nikkei_225/werte',
+    'https://www.finanzen.net/index/hang_seng/werte'
   ]
 }
